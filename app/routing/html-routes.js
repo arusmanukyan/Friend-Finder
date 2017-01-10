a53df0d0c87837e56aff512964c04d90635fd519
arusmanukyan <manukyan.arus@gmail.com>
2017-01-09 20:56:20 -0800

    add server

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -3,10 +3,10 @@
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
+  res.sendFile(path.join(__dirname, "./app/public/home.html"));
 });
 
-app.get("/questions", function(req, res) {
-  res.sendFile(path.join(__dirname, "questions.html"));
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "./app/public/questions.html"));
 });
 
