0c9e7e9ec58db5d7ba3333ebc4a33e5ee7d4092c
arusmanukyan <manukyan.arus@gmail.com>
2017-01-10 18:44:25 -0800

    package

--- server.js
+++ server.js
@@ -16,7 +16,13 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
+require("./app/routing/api-routes.js")(app);
+require("./app/routing/html-routes.js")(app);
 
 
 
