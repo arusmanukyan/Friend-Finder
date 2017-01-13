caa4cf1814783105b51cef83931b63051502f362
arusmanukyan <manukyan.arus@gmail.com>
2017-01-12 17:36:23 -0800

    ,

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -8,11 +8,11 @@ var path = require('path');
 module.exports = function (app) {
 
 	app.get('/', function (req, res) {
-		res.sendFile(path.join(__dirname + '/../public/home'));
+		res.sendFile(path.join(__dirname, + '/../public/home.html'));
 	});
 
 	app.get('/questions', function (req, res) {
-		res.sendFile(path.join(__dirname + '/../public/questions'));
+		res.sendFile(path.join(__dirname, + '/../public/questions.html'));
 	});
 };
 
